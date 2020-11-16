f5e4c30374c244646d290272527ff92394037b24
devcanas <devcanas@gmail.com>
2020-11-16 19:59:27 +0000

    adds nos logo and links to each logo in the footer

--- src/Components/Footer/index.svelte
+++ src/Components/Footer/index.svelte
@@ -8,13 +8,22 @@
 
 <footer>
     <div class="footer-images">
-        <div>
-            <img src="/images/iti.png" width="120" alt="ITI Larsys" />
-            <img src="/images/INESC-ID-logo.png" width="100" alt="Inesc ID" />
-            <img src="/images/cerena.png" width="140" style="margin-left: 20px" alt="Cerena" />
+        <div class="images-wrapper">
+            <a href="https://iti.larsys.pt" target="_blank">
+                <img src="/images/iti.png" width="120" alt="ITI Larsys" />
+            </a>
+            <a href="https://inesc-id.pt" target="_blank">
+                <img src="/images/INESC-ID-logo.png" width="100" style="margin-left: 20px" alt="Inesc ID" />
+            </a>
+            <a href="https://cerena.ist.utl.pt" target="_blank">
+                <img src="/images/cerena.png" width="140" style="margin-left: 30px" alt="Cerena" />
+            </a>
+            <a href="https://www.nos.pt" target="_blank">
+                <img src="/images/nos.png" width="100" style="margin-left: 20px" alt="Nos" />
+            </a>
         </div>
-        <div>
-            <a href="https://tecnico.ulisboa.pt/pt/">
+        <div class="images-wrapper">
+            <a href="https://tecnico.ulisboa.pt/pt/" target="_blank">
                 <img src="/images/ist.png" width="120" alt="IST" />
             </a>
         </div>
