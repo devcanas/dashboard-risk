8a68a3d057def3b1bbfb61b42dd3df921dd64239
devcanas <devcanas@gmail.com>
2020-11-26 12:02:14 +0000

    Adds filter by concelhos to sah data. closes #4

--- public/sah.php
+++ public/sah.php
@@ -3,14 +3,33 @@
 // looks through data-nos.js file and returns the %Ficaremcasa field
 header("Access-Control-Allow-Origin: *");
 
-function filter($item) {
+if (!(isset($_GET["concelho_name"]) || isset($_GET["date"]))) {
+    http_response_code(400);
+    exit();
+}
+
+$filterFn = isset($_GET["concelho_name"]) ? "filterByConcelho" : "filterDate";
+$mapFn = isset($_GET["concelho_name"]) ? "mapForConcelho" : "mapDate";
+
+function filterDate($item) {
     return $item["Data"] === $_GET["date"];
 }
 
-function map($item) {
+function mapDate($item) {
     $newArray = array();
     $newArray["sah"] = $item["%FicaEmCasa"];
     $newArray["concelho"] = $item["Nome Concelho"];
+    return $newArray;
+}
+
+function filterByConcelho($item) {
+    return $item["Nome Concelho"] === $_GET["concelho_name"];
+}
+
+function mapForConcelho($item) {
+    $newArray = array();
+    $newArray["sah"] = $item["%FicaEmCasa"];
+    $newArray["date"] = $item["Data"];
     return $newArray;
 }
 
@@ -22,10 +41,10 @@ $strJsonFileContents = mb_convert_encoding($strJsonFileContents, 'UTF-8', 'HTML-
 $array = json_decode($strJsonFileContents, true);
 $array = $array["#FicaEmCasa NOS 2020"];
 
-$filtered = array_filter($array, "filter");
+$filtered = array_filter($array, $filterFn);
 $filtered = array_values($filtered);
 
 $accum = array();
-$map = array_map("map", $filtered);
+$map = array_map($mapFn, $filtered);
 
 echo json_encode($map);
